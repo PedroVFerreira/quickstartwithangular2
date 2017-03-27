f9629852b4e1fdf7cab980522b87a7902b2aad06
Pedro <pvictor@ciandt.com>
2017-03-26 23:01:56 -0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import "materialize-css";
+import "angular2-materialize";
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
